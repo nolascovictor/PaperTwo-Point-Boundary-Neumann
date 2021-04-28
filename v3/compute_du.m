bdd018e84fc1968e4163f2af5d0585f33c3b7b37
nolascovictor <64096009+nolascovictor@users.noreply.github.com>
2021-04-28 11:09:22 -0300

    Delete v3 directory

--- v3/compute_du.m
+++
@@ -1,9 +0,0 @@
-function du = compute_du(a, x)
-  m = length(a);
-  ii = 1:(m-1);
-  omega = [1 sqrt(1 + (pi*ii).^2 + (pi*ii).^4)];
-  du = 0;
-  for i = 2:m
-    du = du - (((i-1)*pi) * sqrt(2) * sin((i-1) * pi * x) / omega(i)) * a(i);
-  end
-end
